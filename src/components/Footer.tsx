36399fb408786e3ecacb56bfa079a99c5a3949b3
psg0009 <139065268+psg0009@users.noreply.github.com>
2025-05-17 00:50:13 +0530

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white text-center py-6">
+      <p>© 2025 InsureSpectre. All rights reserved.</p>
+      <div className="mt-2 space-x-4">
+        <a href="#" className="hover:underline">Instagram</a>
+        <a href="#" className="hover:underline">LinkedIn</a>
+      </div>
+    </footer>
+  );
+}
---
+++ src/pages/FAQs.tsx
@@ -0,0 +1,3 @@
+export default function FAQs() {
+  return <div className="p-8 text-white">FAQs Page</div>;
+}
