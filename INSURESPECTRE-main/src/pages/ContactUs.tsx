8a3ddfb538b14bea1e1426ee62967da368c66b1c
psg0009 <139065268+psg0009@users.noreply.github.com>
2025-05-17 00:49:36 +0530

    Add files via upload

---
+++ INSURESPECTRE-main/src/pages/ContactUs.tsx
@@ -0,0 +1,3 @@
+export default function ContactUs() {
+  return <div className="p-8 text-white">ContactUs Page</div>;
+}
---
+++ INSURESPECTRE-main/src/pages/HomePage.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <div className="p-8 text-white">HomePage Page</div>;
+}
