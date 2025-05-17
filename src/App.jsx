d19b550f5c3756235e970b8483eae98b42121666
psg0009 <139065268+psg0009@users.noreply.github.com>
2025-05-17 23:30:27 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import InsuranceChatbot from "./components/InsuranceChatbot";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">InsureSpectre Insurance Advisor</h1>
+      <InsuranceChatbot />
+    </div>
+  );
+}
+export default App;
